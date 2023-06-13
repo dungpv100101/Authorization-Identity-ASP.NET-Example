e1434b65126f171875e8b24fac841d3e35b37f5b
dungpv100101 <dungpv100101@gmail.com>
2023-06-14 01:42:15 +0700

    add my profile feature

---
+++ Core3.1/ViewModel/ViewRolesAndClaimsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Core3._1.ViewModel
+{
+    public class ViewRolesAndClaimsViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+}
